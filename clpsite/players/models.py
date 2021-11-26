691ae8e5d8004be37d68fdb2b46fdee40d24eef3
visinojuancruz <visinojuancruz@gmail.com>
2021-11-26 16:54:50 -0300

    agregado a models Agent, Map, Settings, Setup y Rol

--- clpsite/players/models.py
+++ clpsite/players/models.py
@@ -19,10 +19,22 @@ class Person(models.Model):
     def __str__(self):
         return self.fullname
 
+class Agent(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this setup')
+    name = models.CharField(max_length=20, help_text="Enter a agent name")
+    rol = models.ForeignKey('Rol',on_delete=models.SET_NULL, null=True, help_text="Select a rol for this Agent")
+
+    def __str__(self):
+        return self.name
+
+
+
 class Player(Person):
     nick = models.CharField(max_length=25, help_text="Enter a player nick")
     played_hours = models.IntegerField()
     rank = models.ForeignKey('Rank', on_delete=models.SET_NULL, null=True) 
+    agents_pool = models.ManyToManyField(Agent, help_text="Select a agents to make agents_pool")
+
     def __str__(self):
         return self.nick
    
@@ -35,3 +47,39 @@ class Rank(models.Model):
 
     def __str__(self):
         return self.name
+
+class Map(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this map')
+    name = models.CharField(max_length=20, help_text="Enter a map name")
+
+    def __str__(self):
+        return self.name
+
+
+class Setting(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this setting')
+    sensivility = models.FloatField(help_text="Enter a sens number")
+    dpi = models.IntegerField(help_text="Enter a dpi number")
+
+    def get_absolute_url(self):
+         return reverse('player-detail',args=[str(self.id)])
+
+
+class Setup(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this setup')
+    monitor = models.CharField(max_length=25, help_text="Enter a monitor name")
+    mouse = models.CharField(max_length=25, help_text="Enter a mouse name")
+    mousepad = models.CharField(max_length=25, help_text="Enter a mousepad name")
+    keyboard = models.CharField(max_length=25, help_text="Enter a keyboard name")
+    headset = models.CharField(max_length=25, help_text="Enter a headset name")
+
+    def __str__(self):
+        return self.name
+
+
+class Rol(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this setup')
+    name = models.CharField(max_length=20,help_text="Enter a rol name")
+    def __str__(self):
+        return self.name
+
